b13230a704e5aeb8ebfbbf6830db9c63688c7fd5
Amodttech <aaron@amodttech.com>
2021-02-08 18:06:51 -0500

    dropped/reseeded

--- app/serializers/exhibition_serializer.rb
+++ app/serializers/exhibition_serializer.rb
@@ -1,5 +1,5 @@
 class ExhibitionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :theme, :exhibition_objects
+  attributes :id, :user_id, :name, :description, :theme, :exhibition_objects
 
   def exhibition_objects
     ActiveModel::SerializableResource.new(object.exhibition_objects,  each_serializer: ExhibitionObjectSerializer)
