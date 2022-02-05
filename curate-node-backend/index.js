792c0e9a06e12b15bb76a111abd26a91f1f44e68
Aaron Amodt <aaron@amodttech.com>
2022-02-05 14:43:11 -0500

    node server created

--- curate-node-backend/index.js
+++ curate-node-backend/index.js
@@ -11,3 +11,24 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
 	console.log('server listening on port 8080');
 });
+
+
+
+
+// Requiring the module
+const http = require('http');
+
+// Creating server object
+const server = http.createServer((req, res) => {
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>GeeksforGeeks</title><head>');
+	res.write('<body><h2>Hello from Node.js server!!</h2></body>');
+	res.write('</html>');
+	res.end();
+});
+
+// Server setup
+server.listen(3000, ()=> {
+	console.log("Server listening on port 3000")
+});
