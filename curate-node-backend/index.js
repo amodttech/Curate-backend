af08fa793305ad98a318980093ff5f8eee2a43fe
Aaron Amodt <aaron@amodttech.com>
2022-02-05 14:59:08 -0500

    disabled node server (not needed)

--- curate-node-backend/index.js
+++ curate-node-backend/index.js
@@ -1,3 +1,5 @@
+
+//// Express Version
 // Requiring the module
 const express = require('express');
 const app = express();
@@ -5,6 +7,11 @@ const app = express();
 // Route handling
 app.get('/', (req, res) => {
 	res.send('<h2>Hello from Express.js server!!</h2>');
+});
+
+// Handling '/about' request
+app.get('/about', (req,res) => {
+    res.send('<h2>GeeksforGeeks- Express.js</h2>');
 });
 
 // Server setup
@@ -15,20 +22,36 @@ app.listen(8080, () => {
 
 
 
+///// Node Version
 // Requiring the module
-const http = require('http');
-
-// Creating server object
-const server = http.createServer((req, res) => {
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>');
-	res.write('<head><title>GeeksforGeeks</title><head>');
-	res.write('<body><h2>Hello from Node.js server!!</h2></body>');
-	res.write('</html>');
-	res.end();
-});
-
-// Server setup
-server.listen(3000, ()=> {
-	console.log("Server listening on port 3000")
-});
+// const http = require('http');
+  
+// // Creating server object
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+      
+//     if(url === '/') {
+//         res.write('<html>');
+//         res.write(
+// '<head><title>GeeksforGeeks</title><head>');
+//         res.write(
+// '<body><h2>Hello from Node.js server!!</h2></body>');
+//         res.write('</html>');
+//         return res.end();
+//     }
+      
+//     if(url === '/about') {
+//         res.write('<html>');
+//         res.write(
+// '<head><title>GeeksforGeeks</title><head>');
+//         res.write(
+// '<body><h2>GeeksforGeeks- Node.js</h2></body>');
+//         res.write('</html>');
+//         return res.end();
+//     }
+// });
+  
+// // Server setup
+// server.listen(3000, () => {
+//     console.log("Server listening on port 3000")
+// });
